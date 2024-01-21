d46dbda426a1e18e5fcd7e43387edbf1fd83b26e
Connor Loosemore <connorloosemore@Connors-MacBook-Pro.local>
2024-01-21 15:05:22 -0500

    Created Arduino code for driving intermittent stepper motor

---
+++ Arduino/intermittent_stepper.ino
@@ -0,0 +1,46 @@
+// Define pin connections & motor's steps per revolution
+const int stepPin = 10;
+const int stepsPerFrame = 20; //10 frame/40 perf sprocket
+const int stepsPerPerf = stepsPerFrame / 4;
+
+void setup()
+{
+	// Declare pins as outputs
+	pinMode(stepPin, OUTPUT);
+  
+  //come up to speed
+  for (int i = 1000; i < 5000; i++) {
+      digitalWrite(stepPin, HIGH);
+		  delayMicroseconds(1000000/i);
+		  digitalWrite(stepPin, LOW);
+		  delayMicroseconds(1000000/i);
+    }
+}
+
+void loop()
+{
+  //1,000,000 microseconds in one second
+  //24 frames per second
+  //41,666 microseconds per frame (truncated but will result in less than 1 frame deviation over multiple hours)
+  //31,266 microsecond dwell period per frame
+  //10,400 microsecond moving time
+  //for 20 step frame movement, 10 steps for acceleration and 10 for decceleration
+  //Average pulse period needs to be 10,400/20 = 520
+
+  //Accelerating
+  for (int i = 0; i < stepsPerFrame * 4; i += 8) { //pulse starts at 300, goes to 220, linear average is 260
+    digitalWrite(stepPin, HIGH);
+		delayMicroseconds(300 - i);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(300 - i);
+  }
+  //Deccelerating
+  for (int i = stepsPerFrame * 4; i > 0; i -= 8) { //pulse starts at 220, goes to 300, linear average is 260
+    digitalWrite(stepPin, HIGH);
+		delayMicroseconds(300 - i);
+		digitalWrite(stepPin, LOW);
+		delayMicroseconds(300 - i);
+  }
+	//Rest period for intermittent
+	delayMicroseconds(31266);
+}
